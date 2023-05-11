ebae85b941c0ab3371aae966f878f2d80aecbaa7
acalvino4 <acalvino4@gmail.com>
2023-05-11 16:49:26 -0600

    remove irrelevant comments

--- src/tokenparsers/DefaultBlockTokenParser.php
+++ src/tokenparsers/DefaultBlockTokenParser.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of Twig.
- *
- * (c) Fabien Potencier
- * (c) Armin Ronacher
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace acalvino4\embed\tokenparsers;
 
 use Twig\Node\BlockNode;
